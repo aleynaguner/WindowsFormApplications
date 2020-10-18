96261f6e0d43b832ed14ff6009ff543efdd15d30
Aleyna Güner <aleynaguner@hotmail.com>
2020-10-18 14:38:20 +0300

    5thWeek Homework

---
+++ homeworks/3012_odev/3012_odev/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3012_odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList tür = new ArrayList();
+        ArrayList malzeme = new ArrayList();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Pizzza";
+
+            label1.Text = "Pizza Türleri";
+            label2.Text = "Pizza Malzemeleri";
+
+            button1.Text = "Ekle";
+            button2.Text = "Ekle";
+            button3.Text = "Göster";
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+
+            if (!listBox1.Items.Contains(a) && a != "")
+            {
+                listBox1.Items.Add(a);
+            }
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (string s1 in listBox1.Items)
+            {
+                tür.Add(s1);
+            }
+
+            foreach (string s2 in listBox2.Items)
+            {
+                malzeme.Add(s2);
+            }
+
+            Form2 f2 = new Form2();
+
+            f2.c = tür;
+            f2.d = malzeme;
+
+            f2.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string b = textBox2.Text;
+
+            if (!listBox2.Items.Contains(b) && b != "")
+            {
+                listBox2.Items.Add(b);
+                
+            }
+
+            textBox2.Clear();
+            textBox2.Focus();
+        }
+    }
+}
---
+++ homeworks/3012_odev/3012_odev/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3012_odev
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public ArrayList c,d;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "Pizzanı Seç";
+
+            for(int i=0;i<c.Count;i++)
+            {
+                comboBox1.Items.Add(c[i]);
+            }
+
+            for(int j = 0; j < d.Count; j++)
+            {
+                Button bt = new Button();
+                bt.Height = 21;
+                bt.Width = 240;
+                bt.Top = j * 21;
+                bt.Text = d[j].ToString();
+                bt.BackColor = Color.Green;
+                bt.Click += Bt_Click;
+                panel1.Controls.Add(bt);
+            }
+
+        }
+
+        private void Bt_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+
+            if (bt.BackColor==Color.Green)
+            {
+                bt.BackColor = Color.Red;
+            }
+            
+            else if(bt.BackColor==Color.Red)
+            {
+                bt.BackColor = Color.Green;
+            }
+        
+        }
+    }
+}
